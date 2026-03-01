bcdaf944d5d39e6e07e09feadc20067038d9f3c9
KIITIZ_ <kittikavin1998@gmail.com>
2026-03-01 12:13:19 +0700

    Add rectangle class and area method

--- Template.cpp
+++ Template.cpp
@@ -6,7 +6,23 @@ T product(T a, T b){
     return a * b;
 }
 
+template <class T>
+class rectangle{
+public:
+    T area(T a, T b);
+};
+template <class T>
+T rectangle<T>::area(T a, T b){
+    return a * b;
+};
+
+
 int main(){
+    rectangle<int> rect1;
+    rectangle<double> rect2;
+    cout << "Area of rect1 is: " << rect1.area(5, 6) << endl;
+    cout << "Area of rect2 is: " << rect2.area(2.5, 3.0) << endl;
+
     int a = 5, b = 6;
     double c =2.5, d = 3.5;
     cout << "product of a and b: " << product(a, b) << endl;
